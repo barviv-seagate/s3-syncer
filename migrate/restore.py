bdc5f84073d99bddab08e8d71f6805e2f6dc9120
Bari Arviv <bari.arviv@seagate.com>
2022-03-23 11:25:45 +0200

    Fix files
    Signed-off-by: Bari Arviv <bari.arviv@seagate.com>

--- migrate/restore.py
+++ migrate/restore.py
@@ -1,19 +1,23 @@
 import os
 from subprocess import check_output
 
-source_prefix = ''
-exclude, include = [], []
-region = os.getenv('AWS_DEFAULT_REGION', 'us-east-1')
-source_folder = os.getenv('SOURCE_FOLDER', 'aws-bucket')
-target_bucket = os.getenv('TARGET_BUCKET', 'lyvecloud-bucket')
-endpoint = os.getenv('ENDPOINT', 'https://s3.us-east-1.lyvecloud.seagate.com')
+def main():
+    source_prefix = ''
+    exclude, include = [], []
+    region = os.getenv('AWS_DEFAULT_REGION', 'us-east-1')
+    source_folder = os.getenv('SOURCE_FOLDER', 'aws-bucket')
+    target_bucket = os.getenv('TARGET_BUCKET', 'lyvecloud-bucket')
+    endpoint = os.getenv('ENDPOINT', 'https://s3.us-east-1.lyvecloud.seagate.com')
 
-sync_cmd = f'aws s3 sync {source_folder} s3://{target_bucket}{source_prefix} --region {region} --endpoint-url {endpoint}'
-if len(exclude) > 0:
-    sync_cmd += ' --exclude ' + ' --exclude '.join(exclude)
-if len(include) > 0:
-    sync_cmd += ' --include ' + ' --include '.join(include)
+    sync_cmd = f'aws s3 sync {source_folder} s3://{target_bucket}{source_prefix} --region {region} --endpoint-url {endpoint}'
+    if len(exclude) > 0:
+        sync_cmd += ' --exclude ' + ' --exclude '.join(exclude)
+    if len(include) > 0:
+        sync_cmd += ' --include ' + ' --include '.join(include)
 
-print(f'running {sync_cmd}')
-out = check_output(sync_cmd.split(' '))
-print(f'output:\n {out}')
+    print(f'running {sync_cmd}')
+    out = check_output(sync_cmd.split(' '))
+    print(f'output:\n {out}')
+
+if __name__ == '__main__':
+    main()
